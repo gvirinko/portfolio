64d01da35b91fba728daf9478e5662ab7caef869
Ira Gnativ <irina.gnativ@gmail.com>
2020-04-10 14:16:42 +0200

    Added Projects component which renders data from an array, no styling yet

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Skills from './Skills';
+import Projects from './Project';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Skills />
+      <Projects />
     </div>
   );
 }
