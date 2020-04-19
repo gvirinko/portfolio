1fa1ca49b52d023e5294fe4d34dc2243d4031fa0
Ira Gnativ <irina.gnativ@gmail.com>
2020-04-19 22:14:09 +0200

    Added basic styling to the Projects section

--- src/Project.js
+++ src/Project.js
@@ -1,43 +1,55 @@
-import React from 'react';
-// import {SkillsList, skillSet} from './SkillsList';
+import React from "react";
+import honingLogo from "./images/BONUS.png";
+import newsLogo from "./images/NewsAnalyzer.png";
+import mondrian from "./images/Mondrian.png";
 
 const projectSet = [
-    {
-        title: "Honing",
-        logo: "url",
-        link: "url",
-        github: "url",
-        description: "My pet project"
-    },
-    {
-        title: "News Analyzer",
-        logo: "url",
-        link: "url",
-        github: "url",
-        description: "Finds and analyzes news on the given search word"
-    },
-    {
-        title: "Mondrian",
-        logo: "url",
-        link: "url",
-        github: "url",
-        description: "A picture of Piet Mondrian “Composition With Red, Yellow, And Blue\", \“drawn\” solely with CSS-Flexbox"
-    }
+  {
+    title: "Honing",
+    logo: honingLogo,
+    link: "https://honing.pl/",
+    address: "honing.pl",
+    github: "https://github.com/honingpl/honingpl.github.io",
+    description: "A landing page for my parents' beekeeping business",
+  },
+  {
+    title: "News Analyzer",
+    logo: newsLogo,
+    link: "https://gvirinko.github.io/news_analyzer/",
+    github: "https://github.com/gvirinko/news_analyzer",
+    description: "Finds and analyzes news on the given search word",
+  },
+  {
+    title: "Mondrian",
+    logo: mondrian,
+    link: "url",
+    github: "https://github.com/gvirinko/mondrian",
+    description:
+      'A picture of Piet Mondrian “Composition With Red, Yellow, And Blue", “drawn” solely with CSS-Flexbox',
+  },
 ];
 
 // This section is in progress
 const Projects = () => {
-    return (
-        projectSet.map(item => (
-            <div className="project" key={item.title}>
-                <h2 className="project__title">{item.title}</h2>
-                <img src={item.logo} />
-                <a href={item.link}>Link to project</a>
-                <a href={item.github}>Link to the project GitHub</a>
-                <p>{item.description}</p>
-            </div>
-        ))
-    )
+  return projectSet.map((item) => (
+    <div className="project" key={item.title}>
+      <div className="project__wrapper">
+        <img className="project__logo" src={item.logo} alt="Logo" />
+        <div className="project__info-wrapper">
+          <button className="project__button">
+            Website
+            {/* <a href={item.link}>Website</a> */}
+          </button>
+          <button className="project__button">GitHub</button>
+          {/* <a href={item.github}>GitHub</a> */}
+        </div>
+        <div className="project__description-wrapper">
+          <h2 className="project__title">{item.title}</h2>
+          <div className="project__description">{item.description}</div>
+        </div>
+      </div>
+    </div>
+  ));
 };
 
 export default Projects;
