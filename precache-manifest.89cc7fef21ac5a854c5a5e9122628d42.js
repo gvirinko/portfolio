e25aebfac4a74a8ee7b9bdbb2aa23b38f3514eea
Ira Gnativ <irina.gnativ@gmail.com>
2020-06-12 11:33:41 +0200

    Updates

---
+++ precache-manifest.89cc7fef21ac5a854c5a5e9122628d42.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9011c0d884dd5e7921d1570ed69b70d6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4e62fffbd8869e588c87",
+    "url": "/portfolio/static/css/main.758b30a0.chunk.css"
+  },
+  {
+    "revision": "ac063f398ed4c9846429",
+    "url": "/portfolio/static/js/2.c6105693.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.c6105693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e62fffbd8869e588c87",
+    "url": "/portfolio/static/js/main.3bc0e671.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "fed5f226b41f88f954c0061b614e392b",
+    "url": "/portfolio/static/media/BONUS.fed5f226.png"
+  },
+  {
+    "revision": "1c582a00ca708a91abfb789d0bde71d0",
+    "url": "/portfolio/static/media/IMG_6501.1c582a00.jpeg"
+  },
+  {
+    "revision": "bb2cc5c7788ed6e0b078ebe536cfd03c",
+    "url": "/portfolio/static/media/LI-In-Bug.bb2cc5c7.png"
+  },
+  {
+    "revision": "134ffe9809463c3350c8009faee35df6",
+    "url": "/portfolio/static/media/Mondrian.134ffe98.png"
+  },
+  {
+    "revision": "55c1c3e3c1b5fa8354420d079f3394ba",
+    "url": "/portfolio/static/media/NewsAnalyzer.55c1c3e3.png"
+  },
+  {
+    "revision": "fcdd75bd8f10b249d6b4b8a1f76727b7",
+    "url": "/portfolio/static/media/email-logo-png-1121.fcdd75bd.png"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/portfolio/static/media/glyph-logo_May2016.0e84d592.png"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/portfolio/static/media/roboto-regular-webfont.58bf8e0b.woff"
+  },
+  {
+    "revision": "594bcf8bf5ddbb7302400b1319270585",
+    "url": "/portfolio/static/media/robotoslab-regular-webfont.594bcf8b.woff"
+  }
+]);
