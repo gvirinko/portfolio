1751a7adef678ea6244355c95cb4eb165f268a9e
Ira Gnativ <irina.gnativ@gmail.com>
2020-06-18 18:17:19 +0200

    Updates

---
+++ precache-manifest.e56e3d3bccccf5fcfd396c4a87198cbe.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fceb470fc91026d0e6f3d9c4148513cb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d27f6c5d1b85c9973c7a",
+    "url": "/portfolio/static/css/main.f76f0480.chunk.css"
+  },
+  {
+    "revision": "4e13d89d2c666610fcf3",
+    "url": "/portfolio/static/js/2.59fd9572.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.59fd9572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27f6c5d1b85c9973c7a",
+    "url": "/portfolio/static/js/main.1e9f984b.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "fed5f226b41f88f954c0061b614e392b",
+    "url": "/portfolio/static/media/BONUS.fed5f226.png"
+  },
+  {
+    "revision": "1c582a00ca708a91abfb789d0bde71d0",
+    "url": "/portfolio/static/media/IMG_6501.1c582a00.jpeg"
+  },
+  {
+    "revision": "bb2cc5c7788ed6e0b078ebe536cfd03c",
+    "url": "/portfolio/static/media/LI-In-Bug.bb2cc5c7.png"
+  },
+  {
+    "revision": "134ffe9809463c3350c8009faee35df6",
+    "url": "/portfolio/static/media/Mondrian.134ffe98.png"
+  },
+  {
+    "revision": "55c1c3e3c1b5fa8354420d079f3394ba",
+    "url": "/portfolio/static/media/NewsAnalyzer.55c1c3e3.png"
+  },
+  {
+    "revision": "8d5f233717b23342d736222474a436cd",
+    "url": "/portfolio/static/media/Resume(Gnativ).8d5f2337.pdf"
+  },
+  {
+    "revision": "fcdd75bd8f10b249d6b4b8a1f76727b7",
+    "url": "/portfolio/static/media/email-logo-png-1121.fcdd75bd.png"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/portfolio/static/media/glyph-logo_May2016.0e84d592.png"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/portfolio/static/media/roboto-regular-webfont.58bf8e0b.woff"
+  },
+  {
+    "revision": "594bcf8bf5ddbb7302400b1319270585",
+    "url": "/portfolio/static/media/robotoslab-regular-webfont.594bcf8b.woff"
+  }
+]);
