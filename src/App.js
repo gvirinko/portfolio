9ff1d2aad6860c6ce4a6d213943107bb52796f26
Ira Gnativ <irina.gnativ@gmail.com>
2020-06-18 18:21:15 +0200

    Changed projects route

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
             <Switch>
               <Route
                 exact
-                path="/"
+                path="/portfolio"
                 render={(props) => (
                   <Projects {...props} content={projectsList} />
                 )}
